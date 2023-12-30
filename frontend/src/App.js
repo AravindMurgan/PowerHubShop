aec023d7cd2628a1fbbeea6fa2413a4c60b498a6
AravindMurugan <mi.arvindmurugan@gmail.com>
2023-12-30 11:53:43 +0000

    react-router-bootstrap for Links in bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,19 @@ import React from 'react'
 import Header from './components/Header'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-    <Header />
-   
-    <main className='py-3'>
-      <Container>
-      <HomeScreen />
-      </Container>
-    </main>
-    <Footer/>
+      <Header />
+
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
