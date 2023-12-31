6f8f8e43cbcc85597933b75d16e813c58bf6c6ec
AravindMurugan <mi.arvindmurugan@gmail.com>
2023-12-31 10:12:07 +0000

    backend added to project - node js nodemon,dotenv,concurrently,scripts added to server.js

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -39,10 +39,42 @@ const ProductScreen = () => {
                         <ListGroup.Item>
                             Price : ${product.price}
                         </ListGroup.Item>
+                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
                     </ListGroup>
                 </Col>
 
                 <Col md={3}>
+                    <Card>
+                        <ListGroup>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        <strong>{product.countInStock > 0 ? 'In Stock' :
+                                            'Out Of Stock'}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button
+                                    className='btn-block'
+                                    disabled={product.countInStock === 0}
+
+                                >
+                                    Add To Cart
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+
+                    </Card>
 
                 </Col>
             </Row>
