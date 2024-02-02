b75240d90c0aa1fb67f8ae41071a9fa995a6b5b9
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-02-02 08:43:53 +0000

    Cart Qty added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../assets/logo.png'
 import { LinkContainer } from 'react-router-bootstrap'
-
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+    const {cartItems}=useSelector(state=> state.cart);
+    console.log('cartItems',cartItems);
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect >
@@ -19,7 +21,13 @@ const Header = () => {
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto'>
-                            <Nav.Link href='/cart'><FaShoppingCart /> Cart</Nav.Link>
+                            <Nav.Link href='/cart'><FaShoppingCart /> Cart
+                            {cartItems.length > 0 && (
+                                <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                                    {cartItems.reduce((acc,item)=> acc+item.qty,0)}
+                                </Badge>
+                            )}
+                            </Nav.Link>
                             <Nav.Link href='/login'><FaUser /> Sign-In</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
