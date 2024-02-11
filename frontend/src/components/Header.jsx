f9fbf3a568f130286329b93f7a737d241a65b418
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-02-11 23:31:34 +0000

    header changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import { Navbar, Nav, Container, Badge } from 'react-bootstrap'
+import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../assets/logo.png'
 import { LinkContainer } from 'react-router-bootstrap'
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
+// import {logout} from '../../a';
+import { logout } from '../slices/authSlice';
+import { useLogoutMutation } from '../slices/usersApiSlice'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
     const {cartItems}=useSelector(state=> state.cart);
-    console.log('cartItems',cartItems);
+    const {userInfo}=useSelector(state=> state.auth);
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    const [logoutApiCall]=useLogoutMutation();
+
+    const logoutHandler=async ()=>{
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
     return (
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect >
@@ -21,14 +39,35 @@ const Header = () => {
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ms-auto'>
-                            <Nav.Link href='/cart'><FaShoppingCart /> Cart
-                            {cartItems.length > 0 && (
-                                <Badge pill bg='success' style={{marginLeft:'5px'}}>
-                                    {cartItems.reduce((acc,item)=> acc+item.qty,0)}
-                                </Badge>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <FaShoppingCart /> Cart
+                                    {cartItems.length > 0 && (
+                                        <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                                            {cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                                        </Badge>
+                                    )}
+                                </Nav.Link>
+                            </LinkContainer>
+                            {userInfo ? (
+                               <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item>
+                                        Profile
+                                    </NavDropdown.Item>
+                                </LinkContainer>
+
+                                <NavDropdown.Item onClick={logoutHandler}>
+                                    Logout
+                                </NavDropdown.Item>
+                               </NavDropdown>
+
+                            ):(
+                                <LinkContainer to='/login'>
+                                    <Nav.Link><FaUser /> Sign-In</Nav.Link>
+                                </LinkContainer>
+                            
                             )}
-                            </Nav.Link>
-                            <Nav.Link href='/login'><FaUser /> Sign-In</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
