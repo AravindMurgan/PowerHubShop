87c8a0ba7a3be178a8e2b44b8461865f85925252
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-06-07 19:29:48 +0100

    CORS

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,16 @@ import userRoutes from './routes/userRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const port = process.env.PORT || 5000;
 
 connectDb();
 
+
+app.use(cors({
+  origin: "https://power-hub-shop-backend.vercel.app",
+  credentials: true
+}));
 
 const app = express();
 app.use(express.json());
