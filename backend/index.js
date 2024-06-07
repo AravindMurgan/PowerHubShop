93c961ad8f71db4ed1054714b229615e8e378238
AravindMurugan <mi.arvindmurugan@gmail.com>
2024-06-07 19:33:14 +0100

    removed cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import userRoutes from './routes/userRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+///import cors from 'cors';
 const port = process.env.PORT || 5000;
 
 connectDb();
